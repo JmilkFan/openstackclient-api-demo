60d4f2eac6beab4ebd958ac3dfe666e06d3017eb
jmilkfan <fangui_ju@163.com>
2017-01-18 03:28:06 +0800

    Implemented openstack-client demo

---
+++ auto_dep.py
@@ -0,0 +1,115 @@
+import sys
+import os
+from os import path
+import time
+
+import openstack_clients as os_cli
+
+AUTH_URL = 'http://200.21.18.3:35357/v2.0/'
+USERNAME = 'admin'
+PASSWORD = 'fanguiju'
+PROJECT_NAME = 'admin'
+
+DISK_FORMAT = 'qcow2'
+IMAGE_NAME = 'redhat6.5_x86_64_cloud'
+IMAGE_PATH = path.join(path.curdir, 'images', '.'.join([IMAGE_NAME, DISK_FORMAT]))
+
+MIN_DISK_SIZE_GB = 20
+
+KEYPAIR_NAME = 'jmilkfan-keypair'
+KEYPAIT_PUB_PATH = '/home/stack/.ssh/id_rsa.pub'
+
+DB_NAME = 'blog'
+DB_USER = 'wordpress'
+DB_PASS = 'fanguiju'
+
+
+class AutoDep(object):
+    def __init__(self, auth_url, username, password, tenant_name):
+        self.openstack_clients = os_cli.OpenstackClients(
+            auth_url,
+            username,
+            password,
+            tenant_name)
+        self.glance = self.openstack_clients.get_glance_client()
+        self.nova = self.openstack_clients.get_nova_client()
+    
+    def upload_image_to_glance(self):
+        images = self.glance.images.list()
+        for image in images:
+           if image.name == IMAGE_NAME:
+               return image
+        new_image = self.glance.images.create(name=IMAGE_NAME,
+                                              disk_format=DISK_FORMAT,
+                                              container_format='bare',
+                                              min_disk=MIN_DISK_SIZE_GB,
+                                              visibility='public')
+        # Open image file with read+binary.
+        self.glance.images.upload(new_image.id, open(IMAGE_PATH, 'rb'))
+        time.sleep(5)
+        image = self.glance.images.get(new_image.id)
+        return image
+    
+    def get_flavor_id(self):
+        flavors = self.nova.flavors.list()
+        for flavor in flavors:
+            if flavor.disk == MIN_DISK_SIZE_GB:
+                return flavor.id
+    
+    def get_ssh_pub_key(self):
+        if not path.exists(KEYPAIT_PUB_PATH):
+            raise
+        return open(KEYPAIT_PUB_PATH, 'rb').read()
+        
+    def import_keypair_to_nova(self):
+        keypairs = self.nova.keypairs.list()
+        keypair_names = []
+        for keypair in keypairs:
+            keypair_names.append(keypair.name)
+        if KEYPAIR_NAME not in keypair_names:
+            keypair_pub = get_ssh_pub_key()
+            self.nova.keypairs.create(KEYPAIR_NAME, public_key=keypair_pub)
+        
+    def nova_boot(self, image):
+        flavor_id = self.get_flavor_id()
+        self.import_keypair_to_nova()
+
+        db_script_path = path.join(path.curdir, 'scripts/db_server.txt')
+        db_script = open(db_script_path, 'r').read()
+        db_script = db_script.format(DB_NAME, DB_NAME, DB_USER, DB_PASS)
+        import pdb
+        pdb.set_trace()
+        db_instance = self.nova.servers.create(
+            'RHEL-65-MYSQL',
+            image.id,
+            flavor_id,
+            key_name=KEYPAIR_NAME,
+            userdata=db_script)
+        time.sleep(10)
+
+        db_instance_ip = nova.servers.get(db_instance.id).networks['db_network'][0]
+        blog_script_path = path.join(path.curdir, 'script.blog_server.txt')
+        blog_script = open(blog_script_path, 'r').read()
+        blog_script = blog_script.format(DB_NAME, DB_USER, DB_PASS, db_instance_ip)
+        blog_instance = self.nova.servers.create(
+            'RHEL-65-WORDPRESS',
+            image.id,
+            flavor_id,
+            key_name=KEYPAIR_NAME,
+            userdata=blog_script)
+        time.sleep(20)
+
+        servers = self.nova.servers.list(search_opts={'all_tenants':True})
+
+def main(argv):
+    os.environ['LANG'] = 'en_US.UTF8'
+
+    deploy = AutoDep(auth_url=AUTH_URL,
+                     username=USERNAME,
+                     password=PASSWORD,
+                     tenant_name=PROJECT_NAME)
+    image = deploy.upload_image_to_glance()
+    deploy.nova_boot(image)
+
+if __name__ == '__main__':
+     main(sys.argv)
---
+++ openstack_clients.py
@@ -0,0 +1,47 @@
+from openstackclient.identity.client import identity_client_v2
+from keystoneclient import session as identity_session
+import glanceclient
+import novaclient.client as novaclient
+
+
+# FIXME(JmilkFan): Using oslo_config
+NOVA_CLI_VER = 2
+GLANCE_CLI_VER = 2
+
+
+class OpenstackClients(object):
+    """Clients generator of openstack."""
+
+    def __init__(self, auth_url, username, password, tenant_name):
+        ### Identity authentication via keystone v2
+        # An authentication plugin to authenticate the session with.
+        auth = identity_client_v2.v2_auth.Password(
+            auth_url=auth_url,
+            username=username,
+            password=password,
+            tenant_name=tenant_name)
+
+        try:
+            self.session = identity_session.Session(auth=auth)
+        except Exception as err:
+            raise
+
+        # Return a token as provided by the auth plugin.
+        self.token = self.session.get_token()
+        
+    def get_glance_client(self, interface='public'):
+        """Get the glance-client object."""
+        # Get an endpoint as provided by the auth plugin.
+        glance_endpoint = self.session.get_endpoint(service_type="image",
+                                                    interface=interface)
+        # Client for the OpenStack Images API.
+        glance_client = glanceclient.Client(GLANCE_CLI_VER,
+                                            endpoint=glance_endpoint,
+                                            token=self.token)
+        return glance_client
+
+    def get_nova_client(self):
+        """Get the nova-client object."""
+        # Initialize client object based on given version. Don't need endpoint.
+        nova_client = novaclient.Client(NOVA_CLI_VER, session=self.session)
+        return nova_client
